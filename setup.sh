918998243444cc27210d6f2d1ea0b8d9d5f37494
Rando Õispuu <rando@innofly.eu>
2019-09-05 16:38:26 +0300

    remove sudo, add root check

--- setup.sh
+++ setup.sh
@@ -1,41 +1,46 @@
 #!/bin/bash
 
-sudo DEBIAN_FRONTEND=noninteractive
+if [ "$EUID" -ne 0 ]
+  then echo "Please run as root"
+  exit
+fi
+
+DEBIAN_FRONTEND=noninteractive
 
 #Update
-sudo apt update -y
-sudo apt upgrade -y --force-yes
+apt update -y
+apt upgrade -y --force-yes
 
 #Add repos
-sudo apt-add-repository ppa:nginx/stable -y
-sudo apt-add-repository ppa:ondrej/php -y
+apt-add-repository ppa:nginx/stable -y
+apt-add-repository ppa:ondrej/php -y
 curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
 curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
-sudo apt-get install software-properties-common -y
-sudo apt-key adv --recv-keys --keyserver hkp://keyserver.ubuntu.com:80 0xF1656F24C74CD1D8
-sudo add-apt-repository 'deb [arch=amd64,arm64,ppc64el] http://mirror.netinch.com/pub/mariadb/repo/10.4/ubuntu bionic main'
+apt-get install software-properties-common -y
+apt-key adv --recv-keys --keyserver hkp://keyserver.ubuntu.com:80 0xF1656F24C74CD1D8
+add-apt-repository 'deb [arch=amd64,arm64,ppc64el] http://mirror.netinch.com/pub/mariadb/repo/10.4/ubuntu bionic main'
 
-sudo apt-get update -y
+apt-get update -y
 
 #Install packages
-sudo apt install mariadb-server nginx php7.3-mbstring php7.3-xml php7.3-bcmath php7.3-fpm php7.3-mysql php7.3-zip unzip nodejs yarn -y
+apt install mariadb-server nginx php7.3-mbstring php7.3-xml php7.3-bcmath php7.3-fpm php7.3-mysql php7.3-zip unzip nodejs yarn -y
 
 #Install Composer
 cd ~
 curl -sS https://getcomposer.org/installer -o composer-setup.php
-sudo php composer-setup.php --install-dir=/usr/local/bin --filename=composer
+php composer-setup.php --install-dir=/usr/local/bin --filename=composer
 
 #Create Laravel Project
 composer create-project --prefer-dist laravel/laravel
 
 #Move Laravel to correct location
-sudo mv laravel /var/www/
-sudo chown -R www-data.www-data /var/www/laravel/storage
-sudo chown -R www-data.www-data /var/www/laravel/bootstrap/cache
+mv laravel /var/www/
+chown -R www-data.www-data /var/www/laravel/storage
+chown -R www-data.www-data /var/www/laravel/bootstrap/cache
 
 #Configute nginx
-sudo cat > /etc/nginx/sites-available/laravel <<'EOF'
+cat > /etc/nginx/sites-available/laravel <<'EOF'
 server {
     listen 80;
     server_name localhost;
@@ -71,15 +76,15 @@ server {
 }
 EOF
 
-sudo ln -s /etc/nginx/sites-available/laravel /etc/nginx/sites-enabled/
-sudo unlink /etc/nginx/sites-enabled/default
+ln -s /etc/nginx/sites-available/laravel /etc/nginx/sites-enabled/
+unlink /etc/nginx/sites-enabled/default
 
 #Start services
-sudo service nginx start
-sudo service mysql start
-sudo service php7.3-fpm start
+service nginx start
+service mysql start
+service php7.3-fpm start
 
-sudo mysql -u root <<EOF
+mysql -u root <<EOF
 UPDATE mysql.user SET plugin = 'mysql_native_password' WHERE user = 'root' AND plugin = 'unix_socket';
 FLUSH PRIVILEGES;
 EOF
