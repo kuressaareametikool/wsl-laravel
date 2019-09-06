204f5214e7ac659183ba4a9c1c922dada917ef47
Rando Õispuu <rando@innofly.eu>
2019-09-06 15:14:10 +0300

    Add SQL file, add choices

--- setup.sh
+++ setup.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-sudo 'export DEBIAN_FRONTEND=noninteractive'
+read -p "Do you wish use Laravel?" answer
+if [ "$answer" != "${answer#[Yy]}" ] ;then
+    read -p "Do you also want install Laravel?" install
+fi
+
+export DEBIAN_FRONTEND=noninteractive
 
 #Update
 sudo apt update -y
@@ -23,29 +28,38 @@ cd ~
 curl -sS https://getcomposer.org/installer -o composer-setup.php
 sudo php composer-setup.php --install-dir=/usr/local/bin --filename=composer
 
-#Create Laravel Project
-composer create-project --prefer-dist laravel/laravel
-
-#Move Laravel to correct location
-sudo mv laravel /var/www/
-sudo chown -R $USER.www-data /var/www/laravel
-sudo chown -R www-data.www-data /var/www/laravel/storage
-sudo chown -R www-data.www-data /var/www/laravel/bootstrap/cache
-
-#Configute nginx
-sudo mv ~/wsl-laravel/laravel /etc/nginx/sites-available/
-sudo ln -s /etc/nginx/sites-available/laravel /etc/nginx/sites-enabled/
 sudo unlink /etc/nginx/sites-enabled/default
+
+if [ "$answer" != "${answer#[Yy]}" ] ;then
+    if [ "$install" != "${install#[Yy]}" ] ;then
+        #Create Laravel Project
+        composer create-project --prefer-dist laravel/laravel
+    else
+        mkdir laravel/public
+    fi
+
+    #Move Laravel to correct location
+    sudo mv laravel /var/www/
+    sudo chown -R www-data.www-data /var/www/laravel/storage
+    sudo chown -R www-data.www-data /var/www/laravel/bootstrap/cache
+
+    #Configute nginx
+    sudo mv ~/wsl-laravel/laravel /etc/nginx/sites-available/
+    sudo ln -s /etc/nginx/sites-available/laravel /etc/nginx/sites-enabled/
+    cd /var/www/laravel
+else
+    sudo mv ~/wsl-laravel/default /etc/nginx/sites-available/
+    sudo ln -s /etc/nginx/sites-available/default /etc/nginx/sites-enabled/
+    cd /var/www/html
+fi
 
 #Start services
 sudo service nginx start
 sudo service mysql start
 sudo service php7.3-fpm start
 
-mysql -u root <<EOF
+sudo mysql -u root <<EOF
 UPDATE mysql.user SET plugin = 'mysql_native_password' WHERE user = 'root' AND plugin = 'unix_socket';
 FLUSH PRIVILEGES;
 EOF
 exit
-
-cd /var/www/laravel
