115e380b86453f0a371c0447b836716309a2f08e
Rando Õispuu <rando@innofly.eu>
2019-09-07 11:15:36 +0300

    Change script order

--- install.sh
+++ install.sh
@@ -2,10 +2,6 @@
 
 export DEBIAN_FRONTEND=noninteractive
 
-#Update
-apt update -y
-apt upgrade -y
-
 #Add repos
 apt-add-repository ppa:nginx/stable -y
 apt-add-repository ppa:ondrej/php -y
@@ -13,7 +9,8 @@ curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
 curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
 
-apt-get update -y
+apt update -y
+apt upgrade -y
 
 #Install packages
 apt install mariadb-server nginx php7.3-mbstring php7.3-xml php7.3-bcmath php7.3-fpm php7.3-mysql php7.3-zip unzip nodejs yarn -y
@@ -22,3 +19,4 @@ apt install mariadb-server nginx php7.3-mbstring php7.3-xml php7.3-bcmath php7.3
 cd ~
 curl -sS https://getcomposer.org/installer -o composer-setup.php
 php composer-setup.php --install-dir=/usr/local/bin --filename=composer
+php -r "unlink('composer-setup.php');"
